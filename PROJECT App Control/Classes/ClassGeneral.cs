ad9c0bacaa4a9f540d7a226f503a8df0b14512cd
Hakros Interactive <116877505+hakrosinteractive@users.noreply.github.com>
2022-11-01 17:00:39 +0100

    Add files via upload

---
+++ PROJECT App Control/Classes/ClassGeneral.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Windows.Forms;
+
+namespace HAKROS.Classes
+{
+    static class ClassGeneral
+    {
+
+        static public string GetWindowTitle()
+        {
+            return GetAppName() + " " + GetVersion();
+        }
+
+        static public string GetAppName()
+        {
+            return Application.ProductName;
+        }
+
+        static public string GetVersion()
+        {
+            var currentVersion = Assembly.GetExecutingAssembly().GetName().Version;
+            return currentVersion.Major + "." + currentVersion.Minor + "." + currentVersion.Build;
+        }
+
+    }
+
+}
---
+++ PROJECT App Control/Classes/ClassSecurity.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Security.Principal;
+using System.Windows.Forms;
+
+namespace HAKROS.Classes
+{
+    static class ClassSecurity
+    {
+
+        static public bool IsAdministrator()
+        {
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+
+        static public bool ExecuteAsAdmin()
+        {
+            try
+            {
+                var proc = new Process();
+                proc.StartInfo.FileName = Application.ExecutablePath;
+                proc.StartInfo.UseShellExecute = true;
+                proc.StartInfo.Verb = "runas";
+                proc.Start();
+
+                return true;
+
+            }
+            catch
+            {
+                //Error
+            }
+
+            return false;
+
+        }
+
+        static public bool InExecution()
+        {
+            var k = 0;
+            try
+            {
+                var currentname = Assembly.GetExecutingAssembly().GetName().Name.ToLowerInvariant();
+                var ps = Process.GetProcesses();
+                foreach (var p in ps)
+                {
+                    var pname = p.ProcessName.ToLowerInvariant();
+                    if (pname == currentname)
+                    {
+                        k += 1;
+                    }
+                }
+            }
+            catch
+            {
+                //Error!!
+            }
+            return (k >= 2);
+        }
+
+    }
+
+}
